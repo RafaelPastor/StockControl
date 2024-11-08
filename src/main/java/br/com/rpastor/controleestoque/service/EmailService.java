5c4105559a5a848a0e75ce4ada564670855eaeee
Rafael Pastor <rafa.f.pastor@gmail.com>
2024-11-07 21:38:58 -0300

    added EmailService and POM dependencies

---
+++ src/main/java/br/com/rpastor/controleestoque/service/EmailService.java
@@ -0,0 +1,32 @@
+package br.com.rpastor.controleestoque.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true); // true se o conteúdo for HTML
+
+        mailSender.send(message);
+    }
+}
+
